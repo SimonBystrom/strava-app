8afecd2b7842295414b33dcf1069df2e9e0bf3db
simon <simon.bystrom@hyperisland.se>
2022-08-12 18:14:12 +0900

    check 2
    Signed-off-by: simon <simon.bystrom@hyperisland.se>

--- src/components/checkStravaConnection/checkStravaConnection.tsx
+++ src/components/checkStravaConnection/checkStravaConnection.tsx
@@ -1,12 +1,13 @@
 import {FC} from 'react'
 import { StravaData } from "@prisma/client"
 import { useEffect, useState } from "react"
-import { handleLogin } from '../../utils/strava'
+import { getAthlete, handleLogin, reAuthGetter } from '../../utils/strava'
 import { UserStats } from '../userMain/userMain'
 import { trpc } from '../../utils/trpc'
 import { Loader } from '@mantine/core'
 import { asyncLocalStorage } from '../../utils/asyncLocalStorage'
 import { UserActivity } from '../activityMain/activityMain'
+import { useUserStore } from '../../stores/userStore'
 
 type StravaReliantComponents = 'userStats' | 'userActivity'
 
@@ -18,8 +19,45 @@ interface CheckStravaConnectionProps {
 export const CheckStravaConnection: FC<CheckStravaConnectionProps> = ({ userId, checkAgainst }) => {
   const { data: stravaData, isLoading } = trpc.useQuery(['stravaData.getById', { id: userId }])
   const [tokens, setTokens] = useState<StravaData | null>(null)
+  const { setAthlete } = useUserStore()
+  const { mutateAsync } = trpc.useMutation(['stravaData.edit'])
   console.log('Check Strava connection')
   useEffect(() => {
+    const getAthleteData = async (tokenParams: StravaData) => {
+      const expired = new Date(tokenParams.expiresAt * 1000) < new Date()
+      if (!expired) {
+        console.log('Not expired in Checkstravaconnection -> Getting athlete data ....')
+        const athleteData = await getAthlete(tokenParams.accessToken)
+
+        if (!athleteData) {
+          console.log('no athlete for fetch in checkstravaconnection')
+          return
+        }
+        console.log('setting athlete Data to checkstravaconnection', athleteData)
+        setAthlete(athleteData)
+        return
+      }
+      console.log('expierd in checkstravaconnection')
+      const newTokens = await reAuthGetter(tokenParams.refreshToken)
+      if (!newTokens) {
+        console.log('no New tokens found after reAuth in Checkstravaconnection')
+        return
+      }
+      console.log('New tokens from reauth in Checkstravaconnection', newTokens)
+      localStorage.setItem('strava', JSON.stringify(newTokens))
+      await mutateAsync({
+        ...newTokens,
+        userId,
+      })
+      const athleteData = await getAthlete(newTokens.accessToken)
+      if (!athleteData) {
+        console.log('no athlete for refetch in usermain')
+        return
+      }
+      console.log('new AthleteData from userMain', athleteData)
+      setAthlete(athleteData)
+      return
+    }
     console.log('starting check strava connection useEffect')
     const setLocalStorage = async (stravaData: StravaData) => {
       const stravaTokens = {
@@ -32,9 +70,9 @@ export const CheckStravaConnection: FC<CheckStravaConnectionProps> = ({ userId, 
     }
     if (stravaData && !isLoading) {
       console.log('Strava check data if statement loaded -> StravaData found, and trpc client is not loading')
-      setLocalStorage(stravaData)
+      getAthleteData(stravaData)
     }
-  }, [stravaData, isLoading])
+  }, [stravaData, isLoading, mutateAsync, setAthlete, userId])
 
   if (isLoading) {
     return (
