aede7d225db0e2cf0a1beff797c1424fd6f9a7d6
simon <simon.bystrom@hyperisland.se>
2022-11-16 01:02:21 +0900

    almost working iteration strategy for fetching all athlete activities
    Signed-off-by: simon <simon.bystrom@hyperisland.se>

--- src/pages/api/strava/getAthleteActivities.ts
+++ src/pages/api/strava/getAthleteActivities.ts
@@ -1,5 +1,8 @@
 import { StravaData } from "@prisma/client"
 import axios, { AxiosResponse } from "axios"
+import { truncate } from "fs/promises"
+import { request } from "http"
+import { RequestOptions } from "https"
 import { Activity } from "../../../types/stravaTypes"
 import { convertToHourMinSec } from "../../../utils/timeConverter"
 
@@ -28,6 +31,37 @@ type AuthResponse = {
   athleteId: number,
 }
 
+const asyncCalls = async (
+  tokens: AuthResponse,
+  beforeDate?: Date,
+  afterDate?: Date,
+  ) => {
+  let arr_results: any[] | AxiosResponse<any, any> | PromiseLike<AxiosResponse<any, any>> = []
+  const PER_PAGE = 50
+  let page = 1
+  let toContinue = true
+  const beforeDateEpochStamp = beforeDate ? Math.floor(beforeDate.getTime() / 1000) : undefined
+  const afterDateEpochStamp = afterDate ? Math.floor(afterDate.getTime() / 1000) : undefined
+  // Loop over all pages to get all activities.
+  while (toContinue) {
+    let result: AxiosResponse[] = []
+
+    if (page === 1 || result.length === PER_PAGE) {
+      result = await axios.get(
+        `https://www.strava.com/api/v3/athlete/activities?per_page=${PER_PAGE}&page=${page}${beforeDate && `&before=${beforeDateEpochStamp}`}${afterDate && `&after=${afterDateEpochStamp}`}`,
+        { headers: { Authorization: `Bearer ${tokens.accessToken}` } }
+      )
+      page++
+    } else if (result.length < PER_PAGE) {
+      toContinue = false
+    }
+    arr_results = arr_results.concat(result)
+  }
+  return new Promise(resolve => {
+    resolve(arr_results)
+  })
+}
+
 /**
  * Handles fetching user Activities. Used by the useUserActivities hook.
  */
@@ -42,10 +76,9 @@ export const getUserActivities = async (
 
   if (dbTokens) {
     let response: AxiosResponse
-    let activityRes: AxiosResponse
     let tokens: AuthResponse
-    let page = 1
-    let allResultsFetched = false
+
+
     const activities: Activity[] = []
     const expired = new Date(dbTokens.expiresAt * 1000) < new Date()
     if(expired) {
@@ -71,32 +104,11 @@ export const getUserActivities = async (
         athleteId: dbTokens.athleteId
       }
     }
-
-    const beforeDateEpochStamp = beforeDate ? Math.floor(beforeDate.getTime() / 1000) : undefined
-    const afterDateEpochStamp = afterDate ? Math.floor(afterDate.getTime() / 1000) : undefined
-    // Loop over all pages to get all activities.
-    while (!allResultsFetched) {
-      try {
-        activityRes = await axios.get(
-          `https://www.strava.com/api/v3/athlete/activities?per_page=50&page=${page}${beforeDate && `&before=${beforeDateEpochStamp}`}${afterDate && `&after=${afterDateEpochStamp}`}`,
-          { headers: { Authorization: `Bearer ${tokens.accessToken}` } }
-        )
-      } catch (error) {
-        console.log(error)
-        return
-      }
-
-      if (!!activityRes.data.length) {
-        for (const item of activityRes.data) {
-          const activity = parseActivity(item)
-          if (activity) {
-            activities.push(activity)
-          }
-        }
-        page++
-      } else {
-        allResultsFetched = true
-        break
+    let allResults = await asyncCalls(tokens, beforeDate, afterDate)
+    for (const item of allResults.data) {
+      const activity = parseActivity(item)
+      if (activity) {
+        activities.push(activity)
       }
     }
     return {
@@ -108,3 +120,11 @@ export const getUserActivities = async (
   }
   return
 }
+
+
+// for (const item of activityRes.data) {
+//   const activity = parseActivity(item)
+//   if (activity) {
+//     activities.push(activity)
+//   }
+// }
